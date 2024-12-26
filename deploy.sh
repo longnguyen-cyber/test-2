4775a3a054a1d13a49ae32f6d1b0b3e0d94d6b45
longnguyen-cyber <longnguyendev2020@gmail.com>
2024-12-26 21:34:41 +0700

    dd

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-sudo docker stop seim
-sudo docker rm seim
-sudo docker rmi seim
-sudo docker build -t seim:latest .
-sudo docker run -d --network=seim-network --name seim -p 3001:3001 seim
+docker stop seim
+docker rm seim
+docker rmi seim
+docker build -t seim:latest .
+docker run -d --network=seim-network --name seim -p 3001:3001 seim
